890744dd76ae5478d647b8562c554b44e66bd66d
firhansyah13 <firhansyah.dev@gmail.com>
2020-09-25 16:13:19 +0700

    add PerkalianInt

--- perkalian.go
+++ perkalian.go
@@ -1,9 +1,17 @@
-package gomodmultiplies
+package gomodperkalian
 
-func Multiply(items ...int64) int64 {
+func Perkalian(items ...int64) int64 {
 	res := int64(1)
 	for _, item := range items {
 		res *= item
 	}
 	return res
+}
+
+func PerkalianInt(items ...int) int64 {
+	res := 1
+	for _, item := range items {
+		res *= item
+	}
+	return int64(res)
 }
