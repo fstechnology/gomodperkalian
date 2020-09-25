afd664e54e59e7b1b46c8ac9edcd1a206749928d
firhansyah13 <firhansyah.dev@gmail.com>
2020-09-25 18:21:15 +0700

    Major change add convertToInt64

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/fstechnology/gomodperkalian
+module github.com/fstechnology/gomodperkalian/v2
 
 go 1.14
--- perkalian.go
+++ perkalian.go
@@ -1,5 +1,7 @@
 package gomodperkalian
 
+import "reflect"
+
 func Perkalian(items ...int64) int64 {
 	res := int64(1)
 	for _, item := range items {
@@ -14,4 +16,18 @@ func PerkalianInt(items ...int) int64 {
 		res *= item
 	}
 	return int64(res)
+}
+
+func convertToInt64(data interface{}) int64 {
+	if data == nil {
+		return 0
+	}
+	int64Type := reflect.TypeOf(int64(0))
+	v := reflect.ValueOf(data)
+	v = reflect.Indirect(v)
+	if !v.Type().ConvertibleTo(int64Type) {
+		return 0
+	}
+	res := v.Convert(int64Type)
+	return res.Int()
 }
